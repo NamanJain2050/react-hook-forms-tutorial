22e4f7cbe96b0528c58af71cd2954b52b1058946
Naman Jain <iamnaman@amazon.com>
2023-11-24 18:21:36 +0530

    form w react-hook-form

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import FormWithReactHookForm from "@/components/form-w-react-hook-form";
 import FormWithoutReactHookForm from "@/components/form-wo-react-hook-form";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <FormWithoutReactHookForm />
+      {/* <FormWithoutReactHookForm /> */}
+      <FormWithReactHookForm />
     </main>
   );
 }
---
+++ components/form-w-react-hook-form.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import React from "react";
+import { useForm } from "react-hook-form";
+import type { FieldValues } from "react-hook-form";
+
+const FormWithReactHookForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+    getValues,
+  } = useForm();
+
+  const onSubmit = async (data: FieldValues) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-2">
+      <input
+        {...register("email", {
+          required: "Email is required",
+        })}
+        type="email"
+        placeholder="Email"
+        className="px-4 py-2 rounded"
+      />
+      {errors.email && (
+        <p className="text-red-500">{`${errors.email.message}`}</p>
+      )}
+      <input
+        {...register("password", {
+          required: "Password is required",
+          minLength: {
+            value: 10,
+            message: "Password mmust be atleast 10 characters",
+          },
+        })}
+        type="password"
+        placeholder="Password"
+        className="px-4 py-2 rounded"
+      />
+      {errors.password && (
+        <p className="text-red-500">{`${errors.password.message}`}</p>
+      )}
+      <input
+        {...register("confirmPassword", {
+          required: "Confirm password is required",
+          validate: (value) =>
+            value === getValues("password") || "Passwords must match",
+        })}
+        type="password"
+        placeholder="Confirm password"
+        className="px-4 py-2 rounded"
+      />
+      {errors.confirmPassword && (
+        <p className="text-red-500">{`${errors.confirmPassword.message}`}</p>
+      )}
+      <button
+        type="submit"
+        disabled={isSubmitting}
+        className="bg-blue-500 disabled:bg-gray-500 py-2 rounded"
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default FormWithReactHookForm;

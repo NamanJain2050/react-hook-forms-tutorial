d44fdaa5cee6bd2de9d4b9826470cd0f02b039c0
Naman Jain <iamnaman@amazon.com>
2023-11-24 18:40:04 +0530

    form with rhf & zod

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import FormWithReactHookForm from "@/components/form-w-react-hook-form";
+import FormWithReactHookFormAndZod from "@/components/form-w-react-hook-form-zod";
 import FormWithoutReactHookForm from "@/components/form-wo-react-hook-form";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* <FormWithoutReactHookForm /> */}
-      <FormWithReactHookForm />
+      {/* <FormWithReactHookForm /> */}
+      <FormWithReactHookFormAndZod />
     </main>
   );
 }

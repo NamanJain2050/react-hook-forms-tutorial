5124f598e391065d7fc5d228e62559c1e78c51c8
Naman Jain <namanjain2050@gmail.com>
2023-12-13 19:21:24 +0530

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -5,6 +5,9 @@ export const signUpSchema = z
     email: z.string().email(),
     password: z.string().min(10, "Password must be atleast 10 characters"),
     confirmPassword: z.string(),
+    attributes: z.array(
+      z.object({ _id: z.string().min(1), attributeName: z.string().min(1) })
+    ),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords must match",
